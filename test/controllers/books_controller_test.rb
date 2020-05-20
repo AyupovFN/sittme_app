dc2e8c74c6e083e4bc5a2e594589fa28103cfd54
AyupovFN <kapterka@bk.ru>
2020-05-20 21:01:35 +0300

    Added assertions

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -4,15 +4,18 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get books_home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get books_help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get books_about_url
     assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 end
