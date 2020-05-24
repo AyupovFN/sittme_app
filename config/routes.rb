bff2ce145f931e653b5cb629288f35c9b5d27481
AyupovFN <kapterka@bk.ru>
2020-05-24 20:26:41 +0300

    Added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'books#home'
   get 'books/help'
   get 'books/about_me'
+  get 'books/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,21 +1,34 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get books_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get books_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
-  test "should get about" do
+  test "should get about_me" do
     get books_about_me_url
     assert_response :success
-    assert_select "title", "About me | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About me | #{@base_title}"
   end
+
+=begin
+  test "should get contact" do
+    get books_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
+=end
 end
